6dcd8c54ae90250cc4017a6c203dc64a0539ee7f
Bojan Čoka <metropolis.lights@gmail.com>
2015-07-13 11:33:32 +0200

    Encapsulate command execution in a separate class.

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -7,6 +7,10 @@ describe "tourette" do
     JSON.generate({ :command => "echo hello" })
   }
 
+  let(:example_json_reponse) {
+    JSON.generate({ :output => "hello\n" })
+  }
+
   let(:example_invalid_request) {
     "echo hello"
   }
@@ -19,6 +23,9 @@ describe "tourette" do
       end
       it "responds with 200 OK" do
         expect(last_response.status).to eql(200)
+      end
+      it "return the correct response" do
+        expect(last_response.body).to eql(example_json_reponse)
       end
     end
 
