7434da65be2546a1ad0876b051285bcde674a4dc
Bojan Čoka <metropolis.lights@gmail.com>
2015-07-14 12:03:24 +0200

    Modify response status for invalid requests.

--- app.rb
+++ app.rb
@@ -9,7 +9,11 @@ end
 post "/" do
   content_type :json
   headers "Access-Control-Allow-Origin"  => "*"
-  parsedRequest = JSON.parse(request.body.read)
+  begin
+    parsedRequest = JSON.parse(request.body.read)
+  rescue
+    return status 400
+  end
   commandOutput = CommandExecutor.execute(parsedRequest["command"])
   JSON.generate({ :output => commandOutput })
 end
