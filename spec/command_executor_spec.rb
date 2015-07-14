2499edb319d8b1fd8d2cad27988549f8d6d284a6
Bojan Čoka <metropolis.lights@gmail.com>
2015-07-14 11:51:50 +0200

    Use RSpec's "described_class" helper.

--- spec/command_executor_spec.rb
+++ spec/command_executor_spec.rb
@@ -7,7 +7,7 @@ describe CommandExecutor do
     context "given 'echo hello'" do
       
       it "returns 'hello'" do
-        expect(CommandExecutor.execute("echo hello")).to eql("hello\n")
+        expect(described_class.execute("echo hello")).to eql("hello\n")
       end
 
     end
@@ -15,7 +15,7 @@ describe CommandExecutor do
     context "given 'echo $((2 + 2))'" do
 
       it "returns '4'" do
-        expect(CommandExecutor.execute("echo $((2 + 2))")).to eql("4\n")
+        expect(described_class.execute("echo $((2 + 2))")).to eql("4\n")
       end
 
     end
